242a26c5416664bad1b8640e5b28138108fbb7e6
tostu <torge.stubbe@gmail.com>
2024-11-10 02:05:11 +0100

    init

---
+++ src/main/java/tostu/dddplayground/DddPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package tostu.dddplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tostu/dddplayground/DddPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package tostu.dddplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
