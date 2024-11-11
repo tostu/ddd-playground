4a01b058091f3d6902e29632e39bd7825962a569
tostu <torge.stubbe@gmail.com>
2024-11-11 02:39:42 +0100

    modulith shared module

--- src/main/java/tostu/dddplayground/DddPlaygroundApplication.java
+++ src/main/java/tostu/dddplayground/DddPlaygroundApplication.java
@@ -2,7 +2,11 @@ package tostu.dddplayground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.Modulithic;
 
+@Modulithic(
+        sharedModules = {"tostu.dddplayground.core"}
+)
 @SpringBootApplication
 public class DddPlaygroundApplication {
 
